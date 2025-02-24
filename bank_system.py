d9d9d795e27877a36637709848c3e6a02c68e351
Aayan Aqdas <80333428+aayanaqdas@users.noreply.github.com>
2025-02-24 01:03:01 +0100

    refactor menu display with italicized colors and improve error messages;

--- bank_system.py
+++ bank_system.py
@@ -1,17 +1,19 @@
-import json
-import os
-import random
+import json # Lagring av data
+import os # Lese og skrive i JSON filen
+import random # Generering av random tall for kontonummer
 
 # Fargekoder for terminalutskrift
 RED = "\033[91m"
 GREEN = "\033[92m"
 YELLOW = "\033[93m"
-BLUE = "\033[94m"
-CYAN = "\033[96m"
+BLUE_ITALIC = "\033[3;94m"
 RESET = "\033[0m"  # Tilbakestill farge
 
 # Filnavn for lagring av bankdata
 DATA_FILE = "data.json"
+
+#Lagrer brukernavnet globalt for å hente inn bruker data
+current_user = None 
 
 # Funksjon for å laste inn bankdata fra JSON-fil
 def load_bank_data():
@@ -25,11 +27,9 @@ def save_data(data):
     with open(DATA_FILE, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
 
-current_user = None
-
 # Funksjon for å vise startmenyen
 def print_start_menu():
-    print(f"\n{BLUE}------------------ KLP Bank ------------------")
+    print(f"\n{BLUE_ITALIC}------------------ KLP Bank ------------------")
     print("| 1. Logg inn                                |")
     print("| 2. Opprett ny bruker                       |")
     print(f"----------------------------------------------{RESET}")
@@ -44,18 +44,18 @@ def execute_start_menu_choice(choice):
     elif choice == "2":
         create_user()
     else:
-        print(f"{RED}Ugyldig valg! Vennligst velg et tall fra menyen{RESET}")
+        input(f"{RED}Ugyldig valg! Vennligst velg et tall fra menyen (1-2). Trykk Enter for å prøve igjen{RESET}")
         print_start_menu()
 
 # Funksjon for å vise hovedmenyen
 def print_main_menu():
-    print("\n------------------ KLP Bank ------------------")
+    print(f"\n{BLUE_ITALIC}------------------ KLP Bank ------------------{RESET}")
     print(f"{YELLOW}Velkommen {current_user["name"]} {RESET}")
     print("\n 1. Se saldo                                ")
     print(" 2. Sett inn penger                         ")
     print(" 3. Ta ut penger                            ")
     print(" 4. Logg ut                                 ")
-    print("----------------------------------------------")
+    print(f"{BLUE_ITALIC}----------------------------------------------{RESET}")
 
     menu_choice = input("Skriv inn tall for å velge fra menyen: ")
     execute_main_menu_choice(menu_choice)
@@ -70,11 +70,11 @@ def execute_main_menu_choice(choice):
         withdraw()
     elif choice == "4":
         global current_user
-        current_user = None
+        current_user = None # Reset bruker
         print(f"{YELLOW}Logged out!{RESET}")
         print_start_menu()
     else:
-        print(f"{RED}Ugyldig valg! Vennligst velg et tall fra menyen{RESET}")
+        input(f"{RED}Ugyldig valg! Vennligst velg et tall fra menyen (1-4). Trykk Enter for å prøve igjen{RESET}")
         print_main_menu()
 
 # Funksjon for å logge inn en bruker
@@ -83,7 +83,7 @@ def login():
     bank_data = load_bank_data()  # Henter inn all dataen i JSON-filen
     bank_users = bank_data["users"]  # Henter inn alle brukerne i JSON-filen
 
-    print(f"{BLUE}\n-------------------- Login --------------------{RESET}")
+    print(f"{BLUE_ITALIC}\n-------------------- Login --------------------{RESET}")
     username = input("Tast inn ditt brukernavn for å logge inn: ")
 
     if username in bank_users:
@@ -100,12 +100,12 @@ def create_user():
     bank_users = bank_data["users"]
 
 
-    print(f"{BLUE}\n-------------------- Opprett Bruker --------------------{RESET}")
+    print(f"{BLUE_ITALIC}\n-------------------- Opprett Bruker --------------------{RESET}")
     name = input("Skriv inn fulle navn: ")
     username = input("Skriv inn brukernavn: ").lower()
 
     if username in bank_users:
-        print(f"{RED}Brukernavnet {BLUE}'{username}'{RED} er allerede tatt. Vennligst velg en annen{RESET}")
+        print(f"{RED}Brukernavnet {BLUE_ITALIC}'{username}'{RED} er allerede tatt. Vennligst velg en annen{RESET}")
         create_user()
     else:
         account_number = generate_account_num(bank_users)
@@ -140,7 +140,7 @@ def display_balance(amount, action):
 
     balance = bank_users[current_user["username"]]["balance"]
 
-    print( BLUE + "\n-------------------- Saldo --------------------" + RESET)
+    print( BLUE_ITALIC + "\n-------------------- Saldo --------------------" + RESET)
     if action == "deposit":
         print(f"{GREEN}+{amount}{RESET}")
     elif action == "withdraw":
@@ -159,13 +159,13 @@ def deposit():
 
     current_balance = bank_users[current_user["username"]]["balance"]
 
-    print(f"{BLUE}\n-------------------- Sett inn --------------------{RESET}")
+    print(f"{BLUE_ITALIC}\n-------------------- Sett inn --------------------{RESET}")
     print(f"{YELLOW}{current_user["name"]}")
     print(f"Saldo: {current_balance} kr")
     print(f"Kontonummer: {current_user['account_number']}{RESET}")
     print("\n1. Sett inn penger")
     print("2. Gå tilbake")
-    print(f"{BLUE}-------------------------------------------------{RESET}")
+    print(f"{BLUE_ITALIC}-------------------------------------------------{RESET}")
 
     choice = input("Tast inn ditt valg:  ")
 
@@ -184,7 +184,7 @@ def deposit():
     elif choice == "2":
         print_main_menu()
     else:
-        print(f"{RED}Ugyldig valg! Vennligst velg et tall fra menyen{RESET}")
+        input(f"{RED}Ugyldig valg! Vennligst velg et tall fra menyen. Trykk Enter for å prøve igjen{RESET}")
         deposit()
 
 # Funksjon for å ta ut penger
@@ -195,13 +195,13 @@ def withdraw():
 
     current_balance = bank_users[current_user["username"]]["balance"]
 
-    print(f"{BLUE}\n-------------------- Ta ut --------------------{RESET}")
+    print(f"{BLUE_ITALIC}\n-------------------- Ta ut --------------------{RESET}")
     print(f"{YELLOW}{current_user['name']}")
     print(f"Saldo: {current_balance} kr")
     print(f"Kontonummer: {current_user['account_number']}{RESET}")
     print("\n1. Ta ut penger")
     print("2. Gå tilbake")
-    print(f"{BLUE}-----------------------------------------------{RESET}")
+    print(f"{BLUE_ITALIC}-----------------------------------------------{RESET}")
 
     choice = input("Tast inn ditt valg:  ")
 
